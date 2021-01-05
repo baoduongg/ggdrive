2593603a476b6dd3191932454189189d7580e73d
Bao Duong <duongnb@runsystem.net>
2021-01-05 08:48:37 +0700

    first commit

---
+++ ggdrive/ggdrive/GoogleServices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ggdrive
+{
+    public class GoogleServices
+    {
+        public static readonly string ClientId = "334325008766-fo1fl6hhn4dpsbah751v3shaatst600e.apps.googleusercontent.com";
+        public static readonly string ClientSecret = "WCYF3hM7rcFCmsreK0EzIoCv";
+        string Uri = "https://www.googleapis.com/auth/drive";
+        public GoogleServices()
+        {
+        }
+        public async Task<string> GetAccessToken(string code)
+        {
+            var requestUrl =
+              "https://www.googleapis.com/oauth2/v4/token"
+              + "?code=" + code
+              + "&client_id=" + ClientId
+              //+ "&client_secret=" + ClientSecret
+              + "&redirect_uri=" + Uri
+              + "&grant_type=authorization_code";
+
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.PostAsync(requestUrl, null);
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var accessToken = JsonConvert.DeserializeObject<JObject>(json).Value<string>("access_token");
+
+            return accessToken;
+        }
+    }
+}
---
+++ ggdrive/ggdrive/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Xamarin.Auth;
+using Xamarin.Forms;
+
+namespace ggdrive
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+   
+    }
+}
